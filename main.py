d008b691bd80d5eb101cd1cb563fc791c585f392
PiotrEz <panpiotruzieblo@gmail.com>
2022-07-18 00:29:29 +0200

    "Szybka" poprawka do numerowania elementów. Wcześniej po wygaśnięciu elementu pierwszego, nadpisywaliśmy ostatni obiekt. << Postaram się to ładniej poprawić, po prostu nie dało mi to spać.

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import pika
 import redis
+import main
 
 IP_ADDRESS = '192.168.0.13'
 PORT_REDIS = 6379
 QUEUE_NAME = 'hello'
 TTL = 60
 
-while True:
-        connection = pika.BlockingConnection(
-            pika.ConnectionParameters(host=IP_ADDRESS))
-        channel = connection.channel()
-        channel.queue_declare(queue=QUEUE_NAME)
+firsttime = redis.Redis(host=IP_ADDRESS,port=PORT_REDIS,db=0)
+sizee = firsttime.dbsize()
 
-        def callback(ch, method, properties, body):
-            r = redis.Redis(host=IP_ADDRESS, port=PORT_REDIS, db=0)
-            total_keys = r.dbsize()
-            r.set(total_keys, body, ex=TTL)
-        channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
-        channel.start_consuming()
+def callback(ch, method, properties, body):
+    r = redis.Redis(host=IP_ADDRESS, port=PORT_REDIS, db=0)
+    r.set(main.sizee, body, ex=TTL)
+    main.sizee += 1
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host=IP_ADDRESS))
+channel = connection.channel()
+channel.queue_declare(queue=QUEUE_NAME)
+channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
+channel.start_consuming()
 
