88edf8f03a713a9b435be74ea010a4bc9927452c
PiotrEz <panpiotruzieblo@gmail.com>
2022-07-18 19:00:29 +0200

    1.1 - Dodane argumenty uruchamiania

--- main.py
+++ main.py
@@ -1,25 +1,45 @@
 import pika
 import redis
-import main
+import argparse
+import random
 
-IP_ADDRESS = '192.168.0.13'
-PORT_REDIS = 6379
-QUEUE_NAME = 'hello'
-TTL = 60
+parser = argparse.ArgumentParser()
+parser.add_argument("ip_Redis", help="Redis IP address")
+parser.add_argument("ip_rabbit", help="RabbitMQ IP address")
+parser.add_argument("-q", "--queue", default="hello", help="Redis queue name")
+parser.add_argument("-p", "--app_port", default="3000", help="Define application port")
+parser.add_argument("-rp", "--redis_port", default="6379", help="Redis Port")
+parser.add_argument("-t", "--TTL", default="60", type=int, help="Define TTL, default: 60")
+args = vars(parser.parse_args())
 
-firsttime = redis.Redis(host=IP_ADDRESS,port=PORT_REDIS,db=0)
-sizee = firsttime.dbsize()
+RABBIT_IP = args["ip_rabbit"]
+REDIS_IP = args["ip_Redis"]
+QUEUE_NAME = args["queue"]
+APP_PORT = args["app_port"]
+PORT_REDIS = args["redis_port"]
+TTL = args["TTL"]
+
 
 def callback(ch, method, properties, body):
-    r = redis.Redis(host=IP_ADDRESS, port=PORT_REDIS, db=0)
-    r.set(main.sizee, body, ex=TTL)
-    main.sizee += 1
+    ID = random.random()
+    r.set(ID, body, ex=TTL)
+
+
+r = redis.Redis(host=REDIS_IP,port=PORT_REDIS,db=0)
+size = r.dbsize()
+
+
 
 
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host=IP_ADDRESS))
+    pika.ConnectionParameters(host=RABBIT_IP))
 channel = connection.channel()
 channel.queue_declare(queue=QUEUE_NAME)
 channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
+
+
+
+
+
 
